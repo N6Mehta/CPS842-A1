145f617e8957d611c44b18fe9f9759c129b741e5
n6mehta <nitish.mehta33@gmail.com>
2016-09-27 22:59:03 -0400

    Updates

--- src/A1.java
+++ src/A1.java
@@ -1,30 +1,31 @@
 /*
   Created by Nitish on 9/21/2016.
-  NITISH PRO-TIP: USE HASH-MAPS. WHAT YOU HAVE IS WRONG.
-  Hash-map <word, object> where Object is all things related to that word (i.e. Frequency, Document where it is found)
  */
 import java.io.*;
 import java.util.*;
 
 public class A1 {
-    public static void main(String args[]) throws IOException{
-       Map map1 = new HashMap();
-       Scanner scan = null;
+    public static void main(String args[]) throws IOException {
+        Map map1 = new HashMap();
+        Map<String, List<String>> map2 = new HashMap<>();
+       List<String> val = new ArrayList<String>();
+        Scanner scan = null;
+        Scanner scan2 = null;
+        Scanner scan3 = null;
+        //Scan through cacm.all file for words and count words FOR DICTIONARY
         try {
-            scan = new Scanner(new BufferedReader(new FileReader("C:\\Users\\NituPC\\IdeaProjects\\CPS842-A1\\cacm\\cacm.all"))) ;
-            String st;
-            st = scan.next();
+            scan = new Scanner(new BufferedReader(new FileReader("C:\\Users\\Ni2\\IdeaProjects\\CPS842 A1\\cacm\\cacm.all")));
             String next;
-
-            do{
+            String n;
+            do {
                 next = scan.next();
+                //if next valye is .W or .T take the values after it
+                if (next.equals(".W") || (next.equals(".T"))) {
 
-                if (next.equals(".W"))
-                {
                     next = scan.next();
-                    String n = next.replaceAll("[^a-zA-Z\\s]", "").toLowerCase();
-                    //System.out.println(n);
-                    while(!(next.equals(".B"))) {
+                    //while next value is not .B
+                    while (!(next.equals(".B"))) {
+                        n = next.replaceAll("[^a-zA-z]", "").toLowerCase();
                         if (map1.get(n) == null) map1.put(n, 1);
                         else {
                             int newWord = Integer.valueOf(String.valueOf(map1.get(n)));
@@ -32,28 +33,42 @@ public class A1 {
                             map1.put(n, newWord);
                         }
                         next = scan.next();
-                    }  //st = next;
+                    }
                 }
-            }while(scan.hasNext());
-        }finally {
+            } while (scan.hasNext());
+        } finally {
             if (scan != null) {
                 scan.close();
             }
         }
 
-        PrintStream print = new PrintStream(new FileOutputStream("Document.txt", true), true);
 
-        List<String> listVal = new ArrayList<String>(map1.keySet());
-        Collections.sort(listVal);
-        for (int i = 0; i < listVal.size(); i++) {
-            print.println(listVal.get(i) + " " + map1.get(listVal.get(i)));
+        //Scan through common_words file to remove common words
+        try {
+            scan2 = new Scanner(new BufferedReader(new FileReader("C:\\Users\\Ni2\\IdeaProjects\\CPS842 A1\\cacm\\common_words")));
+            String st;
+            do {
+                st = scan2.next();
+                if (map1.get(st) != null) {
+                    map1.remove(st);
+                }
+
+            } while (scan2.hasNext());
+        } finally {
+            if (scan2 != null) {
+                scan2.close();
+            }
         }
+            //Write hashmap to Dictionary.txt file
+            PrintStream print = new PrintStream(new FileOutputStream("Dictionary.txt", true), true);
+
+            List<String> listVal = new ArrayList<String>(map1.keySet());
+            Collections.sort(listVal);
+            for (int i = 0; i < listVal.size(); i++) {
+                print.println(listVal.get(i) + " " + map1.get(listVal.get(i)));
+            }
 
     }
 
-    public static void text(String message, String name) throws IOException {
-        PrintStream print = new PrintStream(new FileOutputStream(name, true), true);
-        print.println(message);
-        print.close();
-    }
+
 }
